15e3275e16150e40db06c0bc1bf865f3fe9238de
xavierau <xavier.au@gmail.com>
2015-03-09 19:16:04 +0800

    update transactions index table view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,23 +9,23 @@
                         <table class="table" role="table">
                             <thead>
                             <th>Item</th>
-                            <th>Vendor</th>
+                            <th class="hidden-xs">Vendor</th>
                             <th colspan="2" style="padding-left:22px">Amount</th>
-                            <th>Payment</th>
+                            <th class="hidden-xs">Payment</th>
                             <th>edit</th>
                             </thead>
                             <tbody>
                                 @foreach($transactions as $transaction)
                                     <tr>
                                         <td>{{$transaction->item}}</td>
-                                        <td>{{$transaction->vendor}}</td>
+                                        <td class="hidden-xs">{{$transaction->vendor}}</td>
                                         <td style="width: 14px; padding: 8px 0 0;">
                                             @if($transaction->receipt)
                                                 <i class="fa fa-file-o"></i>
                                             @endif
                                         </td>
                                         <td>{{"$".number_format($transaction->amount, 1, '.', ',') }}</td>
-                                        <td>{{$transaction->payment}}</td>
+                                        <td class="hidden-xs">{{$transaction->payment}}</td>
                                         <td>
                                             <a class="btn btn-xs btn-info" href="{{route('transactions.edit', $transaction->id)}}">edit</a>
                                         </td>
