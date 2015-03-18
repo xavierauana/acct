8a38a030642f328801400735eb82ba31a8e643e8
xavierauana <xavier.au@anacreation.com>
2015-03-18 17:51:54 +0800

    modifiy the register link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -52,6 +52,7 @@
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
 						<li><a href="/auth/login">Login</a></li>
+                        <li><a href="/auth/register">Register</a></li>
 					@else
                         <li><a href="/auth/register">Register</a></li>
 						<li class="dropdown">
